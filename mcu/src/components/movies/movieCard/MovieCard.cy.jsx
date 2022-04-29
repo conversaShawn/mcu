0736d3b970ad0215db818d316e322418f8f1ba80
shawnhelps <shawn.harris3510@gmail.com>
2022-04-28 21:11:02 -0700

    image properly importing in component

--- mcu/src/components/movies/movieCard/MovieCard.cy.jsx
+++ mcu/src/components/movies/movieCard/MovieCard.cy.jsx
@@ -8,8 +8,8 @@ describe('check MovieCard', () => {
         cy.mount(<MovieCard />)
         cy.get(".movieCard").should('have.css', 'height', '300px')
     })
-    it('checks css', () => {
-        cy.mount(<MovieCard />)
-        cy.get(".movieCard").contains('Card')
-    })
+    // it('checks css', () => {
+    //     cy.mount(<MovieCard />)
+    //     cy.get(".movieCard").contains('Card')
+    // })
 })
--- mcu/src/components/movies/movieCard/MovieCard.js
+++ mcu/src/components/movies/movieCard/MovieCard.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import "./MovieCard.css"
+import CardMedia from "@mui/material/CardMedia";
+import "./MovieCard.css";
+import image from "../../../images/iron_man.jpeg"
 
 const MovieCard = () => {
-  return <Card className="movieCard" variant="outlined">Card</Card>;
+  return (
+    <Card className="movieCard" variant="outlined">
+      <CardMedia 
+      component="img"
+      // image="../../../../public/iron_man.jpeg"
+      src={image}
+      alt="Iron Man poster"
+      />
+    </Card>
+  );
 };
 
 export default MovieCard;
