3501afabc4448d592f5e9bc99e18416b9a909e18
conversaShawn <shawn.harris3510@gmail.com>
2022-05-01 18:18:11 -0700

    flex wrap works

--- mcu/src/components/movies/movieCard/MovieCard.js
+++ mcu/src/components/movies/movieCard/MovieCard.js
@@ -1,21 +1,50 @@
 import React from "react";
 import "./MovieCard.css";
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
 // import ImageListItem from "@mui/material/ImageListItem";
 
 const MovieCard = () => {
   return (
-    <Card className="test">
+    <Grid className="test" >
       {movieList.map((movie) => (
         <Card key={movie.img}>
           <img src={movie.img} alt={movie.title} />
         </Card>
       ))}
-    </Card>
+    </Grid>
   );
 };
 
 const movieList = [
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
+    title: "X-Men",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/theincrediblehulk_lob_crd_01_2.jpg",
+    title: "Spiderman",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
+    title: "Hulk",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
+    title: "X2: X-Men United",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
+    title: "Spiderman 2",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
+    title: "X-Men: The Last Stand",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
+    title: "Spiderman 3",
+  },
   {
     img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman_lob_crd_01_3.jpg",
     title: "Iron Man",
